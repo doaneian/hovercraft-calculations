f6a4ded4a852c8df05dd8e072c3e6cfa8ffdcab1
ian.doane <ian.doane@gmail.com>
2016-06-26 09:56:17 -0500

    add navbar

--- client/pages/calculation/components/App.js
+++ client/pages/calculation/components/App.js
@@ -5,11 +5,11 @@ require('./App.less');
 
 const App = () => (
   <div className='container center app-block'>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-          <ul class="nav navbar-nav">
-            <li><a href="how-it-works.html">How it Works</a></li>
-            <li><a href="#" class="active">Calculations</a></li>
+    <nav className='navbar'>
+      <div className='container-fluid'>
+          <ul className='nav navbar-nav'>
+            <li><a href='index.html'>How it Works</a></li>
+            <li className='active'><a href='calculations.html'>Calculations</a></li>
           </ul>
       </div>
     </nav>
