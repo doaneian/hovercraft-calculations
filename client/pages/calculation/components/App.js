fb046260f69473e8c146d6cae18da09af111de0c
ian.doane <ian.doane@gmail.com>
2016-06-26 09:04:54 -0500

    fix script tag has to be under the root div

--- client/pages/calculation/components/App.js
+++ client/pages/calculation/components/App.js
@@ -5,6 +5,14 @@ require('./App.less');
 
 const App = () => (
   <div className='container center app-block'>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li><a href="how-it-works.html">How it Works</a></li>
+            <li><a href="#" class="active">Calculations</a></li>
+          </ul>
+      </div>
+    </nav>
     <Inputs />
     <Outputs />
   </div>
