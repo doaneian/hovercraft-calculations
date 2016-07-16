679d718b260ca8d7ee636c8ef1352499d2958517
ian.doane <ian.doane@gmail.com>
2016-07-15 22:50:43 -0500

    add formatting for equations

--- client/pages/calculations-explanation/calculations-explanation.js
+++ client/pages/calculations-explanation/calculations-explanation.js
@@ -20,14 +20,18 @@ const CalculationsExplanation = () => (
       </li>
       <li className='bullet-point'>
         Point A is potential energy from the pressure.  We’re assuming 0 air
-        velocity, which means the kinetic energy is 0.  Therefore, the energy
-        at point A can be represented by <sup>P</sup>&frasl;<sub><i>p</i></sub>
+        velocity, which means the kinetic energy is 0.  This means that
+        <div className='equation'>
+          Energy<sub>point A</sub> = <sup>P</sup>&frasl;<sub><i>p</i></sub>
+        </div>
       </li>
       <li className='bullet-point'>
         Point B is kinetic energy.  We’re assuming the pressure goes to 0
         relative to the pressurized chamber, which means the potential energy
-        is 0.  Therefore, the energy at point B can be represented by
-        <sup>1</sup>&frasl;<sub>2</sub> V<sup>2</sup>
+        is 0.  This means that
+        <div className='equation'>
+          Energy<sub>point B</sub> = <sup>1</sup>&frasl;<sub>2</sub> V<sup>2</sup>
+        </div>
       </li>
     </ul>
     <img src='./assets/calculations-diagram.jpg' />
@@ -35,30 +39,41 @@ const CalculationsExplanation = () => (
       <li className='bullet-point'>
         We can set the potential energy from the pressurized chamber equal
         to the kinetic energy from the nozzle.  Doing this, we get
-        <sup>P</sup>&frasl;<sub><i>p</i></sub> = <sup>1</sup>&frasl;<sub>2</sub> V<sup>2</sup>
+        <div className='equation'>
+          <sup>P</sup>&frasl;<sub><i>p</i></sub> = <sup>1</sup>&frasl;<sub>2</sub> V<sup>2</sup>
+        </div>
         We can now solve for the velocity at point B
       </li>
       <li className='bullet-point'>
         Now that we know the velocity, we can calculate the area of the
         nozzle that is needed to get the thrust that we want.  This equation is
-        F = <i>p</i>V<sup>2</sup>A.
+        <div className='equation'>
+          F = <i>p</i>V<sup>2</sup>A
+        </div>
       </li>
       <li className='bullet-point'>
         Now that we know the velocity of the air coming out of the nozzle as
         well as the area of the nozzle, we can find the flowrate that is necessary
         to sustain the flow coming out of the nozzle while not losing pressure
-        underneath the hovercraft.  Flowrate = V*A
+        underneath the hovercraft.
+        <div className='equation'>
+          Flowrate = V*A
+        </div>
       </li>
       <li className='bullet-point'>
         Blowers are often rated by flowrate at different pressures.  The chart
         provided attempts to provide standardized flowrate vs pressure that can
         be used to find a blower that meets the requirements.  To calculate the
         flowrate at the different pressures, we can use the fact that pressure * volume
-        will always be the same.  This means that P<sub>standardized</sub> *
-        Volume<sub>standardized</sub> = P<sub>calculated</sub> * Volume<sub>calculated</sub>
-        which can be rearranged to Volume<sub>standardized</sub> = (Volume<sub>calculated</sub>
-        * P<sub>calculated</sub>) / (P<sub>standardized</sub>) so we can calculate
-        the volume at each pressure we want to plot.
+        will always be the same.  This means that
+        <div className='equation'>
+          P<sub>standardized</sub> * Volume<sub>standardized</sub> = P<sub>calculated</sub> * Volume<sub>calculated</sub>
+        </div>
+        which can be rearranged to
+        <div className='equation'>
+          Volume<sub>standardized</sub> = (Volume<sub>calculated</sub> * P<sub>calculated</sub>) / (P<sub>standardized</sub>)
+        </div>
+        so we can calculate the volume at each pressure we want to plot.
       </li>
     </ul>
   </div>
